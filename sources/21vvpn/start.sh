0d6fce6c01e3ffda8527cdc9af41a3c1ffc8bb67
Andrew Zong <a.zong@f5.com>
2019-08-14 18:51:33 +0800

    update vpn dockerfile and start.sh

--- sources/21vvpn/start.sh
+++ sources/21vvpn/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 cdir=`cd $(dirname $0); pwd`
-image_name=21vvpn:latest
+image_name=f5-ps-21vvpn:latest
 container_name=VPNC
 
 docker build $cdir -f $cdir/Dockerfile -t $image_name
@@ -11,10 +11,8 @@ pass=$2
 
 docker rm --force $container_name
 docker run \
-    --rm \
     -itd \
     --name $container_name \
-    -v $cdir/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt \
     -e USERNAME=$user -e PASSWORD=$pass \
     --privileged \
     $image_name
