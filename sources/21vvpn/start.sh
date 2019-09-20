73c88657eb5929094e66ae759ec5fa2168f3ab88
Andrew Zong <a.zong@f5.com>
2019-09-20 11:50:51 +0800

    add webssh and squid to 21vvpn

--- sources/21vvpn/start.sh
+++ sources/21vvpn/start.sh
@@ -4,15 +4,21 @@ cdir=`cd $(dirname $0); pwd`
 image_name=zongzw/f5-ps-21vvpn-centos:latest
 container_name=VPNC
 
-[ $# -ne 2 ] && echo "$0 <user> <password>" && exit 1
+[ $# -lt 1 ] && echo "$0 <user> [<password>]" && exit 1
 user=$1
-pass=$2
+if [ $# -eq 2 ]; then 
+    pass=$2
+elif [ $# -eq 1 ]; then 
+    read -s -p "Input Password: " pass
+fi
 
 docker rm --force $container_name
 docker run \
     -itd \
     --name $container_name \
     -e USERNAME=$user -e PASSWORD=$pass \
+    -v $cdir/squid.conf:/etc/squid/squid.conf \
+    -p 3128:3128 -p 8888:8888 \
     --privileged \
     $image_name
 
