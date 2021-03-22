e1c2f7e548dd3eae1ce6c26f1f98ebdd152fe422
Andrew Zong <a.zong@f5.com>
2021-03-22 09:03:25 +0800

    move webssh outside this repo.

--- sources/webssh/gen-certs.sh
+++
@@ -1,5 +0,0 @@
-certsdir=./certs
-mkdir -p $certsdir
-openssl req -newkey rsa:2048 -nodes -keyout $certsdir/domain.key -x509 \
-    -days 365 -out $certsdir/domain.crt \
-    -subj "/C=CN/ST=BJ/L=BJ/O=Example/OU=IT/CN=f5.com"
